d8eebc4f4322374192239c770dd5619ab874e0a6
lav-joshi <lcs2019022@iiitl.ac.in>
2020-05-06 18:00:57 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Navbar from "./components/navbar"
+import India from "./components/india"
+import Home from "./components/home"
+import Countries from "./components/countries"
+import Prevent from "./components/prevent"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="container">
+    <Navbar />
+    <br/>
+    <Route path="/" exact component={Home} />
+    <Route path="/countries/data" component={Countries} />
+    <Route path="/india/state" component={India} />
+    <Route path="/how-to-prevent" component={Prevent} />
     </div>
-  );
+  </Router>
+  )
 }
+
+
+
 
 export default App;
